e01112a999d5865c98b78a2400e9d9fde2d469b1
Jelle Vandebeeck <jelle@fousa.be>
2016-08-19 16:57:59 +0200

    Add some form validations.

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,9 @@
 class Registration < ApplicationRecord
 
+  # Validations
   validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, email: true
+  validates :club, presence: true
+  validates :food, presence: true, numericality: { only_integer: true }
 
 end
